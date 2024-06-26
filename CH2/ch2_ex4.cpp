0f1e77162c45897f4390479de09c67e6545d893e
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-06-26 09:40:00 +0800

    Update ch2_ex4.cpp

--- CH2/ch2_ex4.cpp
+++ CH2/ch2_ex4.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 int main(){
 	
+	int age,hour,min;
 	cout << "Enter your age: ";
-	int age;
 	cin >> age;
 	cout << "Enter the number of hour: ";
-	int hour;
 	cin >> hour;
 	cout << "Enter the number of minutes: ";
-	int min;
 	cin >> min;
 	cout << "Time: " << hour <<":" << min;
 	
