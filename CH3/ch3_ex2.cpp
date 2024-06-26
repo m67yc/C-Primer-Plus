a5b1b397c74a639213fed2f895bb15e4f0d6ad17
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-06-26 17:00:15 +0800

    Add files via upload

---
+++ CH3/ch3_ex2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+const int exchange_foot_to_inch = 12;
+const double exchange_weight = 2.2;
+const double exchange_inch_to_m = 0.0254;
+
+int main(){
+	
+	int height_inch,height_foot,weight_pound;
+	double BMI;
+	cout << "Enter your height in foot and inch:________\b\b\b\b\b\b\b\b";
+	cin >> height_foot;
+	cin >> height_inch;
+	cout << "Enter your weight in pound:________\b\b\b\b\b\b\b\b";
+	cin >> weight_pound;
+	BMI = (weight_pound/exchange_weight)/pow(((height_foot*exchange_foot_to_inch+height_inch)*exchange_inch_to_m),2);
+	cout << "BMI = " << BMI;
+	
+	
+	return 0;
+}
