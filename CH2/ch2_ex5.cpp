ebadc68fca6c0d7f56de3cf312be3e422fbe7741
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-06-26 09:40:35 +0800

    Update ch2_ex5.cpp

--- CH2/ch2_ex5.cpp
+++ CH2/ch2_ex5.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int exchage(int x);
 
 int main(){
+	
+	double celsius_valus,exchange_to_fahrenheit_valus;
 	cout << "Please enter a Celsius valus: ";
-	double celsius_valus;
 	cin >> celsius_valus;
-	double exchange_to_fahrenheit_valus = exchage(celsius_valus);
+	exchange_to_fahrenheit_valus = exchage(celsius_valus);
 	cout << celsius_valus << " degrees Celsius is " << exchange_to_fahrenheit_valus << " degrees Fahrenheit.";
 	
 	return 0;
