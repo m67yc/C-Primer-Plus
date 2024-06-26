a38380feeaede46ecb041151abb029157a270f00
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-06-26 08:59:24 +0800

    Add files via upload

---
+++ CH2/ch2_ex4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout << "Enter your age: ";
+	int age;
+	cin >> age;
+	cout << "Enter the number of hour: ";
+	int hour;
+	cin >> hour;
+	cout << "Enter the number of minutes: ";
+	int min;
+	cin >> min;
+	cout << "Time: " << hour <<":" << min;
+	
+	return 0;
+}
