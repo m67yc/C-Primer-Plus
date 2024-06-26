d9996bd6dca717e70176f0fd1f277c922fe4f22d
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-06-26 09:41:02 +0800

    Update ch2_ex6.cpp

--- CH2/ch2_ex6.cpp
+++ CH2/ch2_ex6.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int exchage(double x);
 
 int main(){
+	
+	double num_LightYear,exchange_to_astronomical_units;
 	cout << "Enter the number of light years: ";
-	double num_LightYear;
 	cin >> num_LightYear;
-	double exchange_to_astronomical_units = exchage(num_LightYear);
+	exchange_to_astronomical_units = exchage(num_LightYear);
 	cout << num_LightYear << " light years " << exchange_to_astronomical_units << " astronomical units.";
 	
 	return 0;
