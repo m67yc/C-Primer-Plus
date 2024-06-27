85e7b62e4971e2ca4f47a186ffe90589ce9341bb
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-06-27 10:16:23 +0800

    Add files via upload

---
+++ CH3/ch3_ex6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	
+	long double distance_mile,gasoline_quantity_gallon,distance_km,gasoline_quantity_L,fuel_consumption_per_100_kilometers;
+	
+	cout << "Enter the distance in mile: ";
+	cin >> distance_mile;
+	cout << "Enter the gasoline quantity in gallon: ";
+	cin >> gasoline_quantity_gallon;
+	gallon_per_fuel_consumption = distance_mile / gasoline_quantity_gallon;
+	cout << gallon_per_fuel_consumption << "gallon per fuel consumption";
+
+	return 0;
+}
