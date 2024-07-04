ae4baa2275ce08a78e47809731604f62ba7c340d
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-04 11:32:05 +0800

    Update ch4_ex3.cpp

--- CH4/ch4_ex3.cpp
+++ CH4/ch4_ex3.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cstring>
+#include <string>
 using namespace std;
 
 int main(){
@@ -7,12 +8,17 @@ int main(){
 	const int ArSize = 30;
 	char first_name[ArSize];
 	char last_name[ArSize];
+	char full_name[ArSize*2];
 	
 	cout << "Enter your first name: ";
 	cin.getline(first_name,ArSize);
 	cout << "Enter your last name: ";
 	cin.getline(last_name,ArSize);
-	cout << "Here's the information in a single string: " << last_name << ", " << first_name;
+	
+	strcpy(full_name, last_name);
+	strcat(full_name, ", ");
+	strcat(full_name, first_name);
+	cout << "Here's the information in a single string: " << full_name;
 	
 	return 0;
 }
