9f325dbc597c609db71542333037272d8c55e410
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-04 10:29:12 +0800

    Update ch4_ex2.cpp

--- CH4/ch4_ex2.cpp
+++ CH4/ch4_ex2.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 int main(){
+    
     string name, dessert;
 
     cout << "Enter your name:\n";
     cin >> name;
     cout << "Enter your favorite dessert:\n";
     cin >> dessert;
-    cout << "I have some delicious" << dessert;
-    cout << "for you," << name << ".\n";
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+    
     return 0;
 }
