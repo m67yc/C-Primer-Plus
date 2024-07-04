e4ca140c55dca42d971966ddbf9b15e71014215e
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-04 12:03:48 +0800

    Add files via upload

---
+++ CH4/ch4_ex9.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+struct CandyBar{
+	
+	string candy_name;
+	int cal;
+	float weight;
+	
+};
+
+int main(){
+	
+	CandyBar * snack = new CandyBar[3];
+	snack[0] = {"Mocha Munch", 350, 2.3};
+	snack[1] = {"A", 300, 3.4};
+	snack[2] = {"B", 250, 4.5};
+	
+	for(int i=0;i<=2;i++){
+		cout << "Candy's name: " << snack[i].candy_name << endl 
+	         << "weight: " << snack[i].weight << endl
+		     << "Calories: " << snack[i].cal << "\n\n";
+	}
+	
+	return 0;
+}
