448b896faaa92ed9bc12d9f12a151db45036d299
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-04 10:22:41 +0800

    Add files via upload

---
+++ CH4/ch4_ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    string name, dessert;
+
+    cout << "Enter your name:\n";
+    cin >> name;
+    cout << "Enter your favorite dessert:\n";
+    cin >> dessert;
+    cout << "I have some delicious" << dessert;
+    cout << "for you," << name << ".\n";
+    return 0;
+}
