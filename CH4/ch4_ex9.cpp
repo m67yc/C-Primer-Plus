12bb9165d90e912af68028e48d3d2fc56ff23896
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-08 11:01:55 +0800

    Update ch4_ex9.cpp

--- CH4/ch4_ex9.cpp
+++ CH4/ch4_ex9.cpp
@@ -10,16 +10,23 @@ struct CandyBar{
 	
 };
 
+
 int main(){
 	
+	//char ch[10] = "abcd"; 
+	//cout << ch << endl;
+	
+	//int array[10] = {1,2,3,4};
+	//cout << array << endl;
+	
 	CandyBar * snack = new CandyBar[3];
-	snack[0] = {"Mocha Munch", 350, 2.3};
-	snack[1] = {"A", 300, 3.4};
-	snack[2] = {"B", 250, 4.5};
+	snack[0] = {"Mocha Munch", 350, 2.3}; //snack[0] = (*snack)
+	snack[1] = {"A", 300, 3.4};			  //snack[1] = *(snack+1)
+	snack[2] = {"B", 250, 4.5};			  //snack[2] = *(snack+2)
 	
 	for(int i=0;i<=2;i++){
-		cout << "Candy's name: " << snack[i].candy_name << endl 
-	         << "weight: " << snack[i].weight << endl
+		cout << "Candy's name: " << (snack+i)->candy_name << endl  
+	         << "weight: " << (*(snack+i)).weight << endl
 		     << "Calories: " << snack[i].cal << "\n\n";
 	}
 	
