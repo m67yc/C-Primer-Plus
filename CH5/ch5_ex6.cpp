d6b7d8ff6fd9047ac54a1fed14d3917d470d12af
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-09 14:21:44 +0800

    Update ch5_ex6.cpp

--- CH5/ch5_ex6.cpp
+++ CH5/ch5_ex6.cpp
@@ -5,17 +5,17 @@ int main(){
 	
 	int num_sold[3][12] = {0};
 	int total;
-	char*year[12] ={"January","February","March","April","May","June","July","August","September","October","November","December"};
+	char *year[12] ={"January","February","March","April","May","June","July","August","September","October","November","December"};
 	
 	for(int i = 0;i < 3;i++){
 		for(int o = 0;o < 12;o++){
-			cout << "Enter the number that C++ For Fools» sold in " << year[i] << "in " << i+1 << " year: ";
+			cout << "Enter the number that C++ For Fools sold in " << year[o] << " in " << i+1 << " year: ";
 			cin >> num_sold[i][o];
 		}
 	}
 	for(int i = 0;i < 3;i++){
 		for(int o = 0;o < 12;o++){
-			cout << "The number that C++ For Fools sold in " << year[i]
+			cout << "The number that C++ For Fools sold in " << year[o]
 		         << "in " << i+1 << " year: " << num_sold[i][o] << endl;
 		    total += num_sold[i][o];
 		}
