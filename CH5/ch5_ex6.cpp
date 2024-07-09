e4649059a8b358ab4162d74208563eb226cf8907
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-09 12:04:22 +0800

    Add files via upload

---
+++ CH5/ch5_ex6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int num_sold[3][12] = {0};
+	int total;
+	char*year[12] ={"January","February","March","April","May","June","July","August","September","October","November","December"};
+	
+	for(int i = 0;i < 3;i++){
+		for(int o = 0;o < 12;o++){
+			cout << "Enter the number that C++ For Fools» sold in " << year[i] << "in " << i+1 << " year: ";
+			cin >> num_sold[i][o];
+		}
+	}
+	for(int i = 0;i < 3;i++){
+		for(int o = 0;o < 12;o++){
+			cout << "The number that C++ For Fools sold in " << year[i]
+		         << "in " << i+1 << " year: " << num_sold[i][o] << endl;
+		    total += num_sold[i][o];
+		}
+	}
+	cout << "The total: " << total;
+	return 0;
+	
+}
