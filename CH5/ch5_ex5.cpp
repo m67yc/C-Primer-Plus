858f138b831ee9bdda9817457308aa4ba0759a9e
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-09 11:55:49 +0800

    Update ch5_ex5.cpp

--- CH5/ch5_ex5.cpp
+++ CH5/ch5_ex5.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main(){
 	
 	int num_sold[12] = {0};
-	char year[12][20] {{"January"},{"February"},{"March"},{"April"},{"May"},{"June"},{"July"},{"August"},{"September"},{"October"},{"November"},{"December"}};
+	int total;
+	char*year[12] ={"January","February","March","April","May","June","July","August","September","October","November","December"};
 	
-	for(int i = 0;i <= 12;i++){
-		cout << "Enter the number that «C++ For Fools» sold in " << year[i] << ": ";
+	for(int i = 0;i < 12;i++){
+		cout << "Enter the number that C++ For Fools?sold in " << year[i] << ": ";
 		cin >> num_sold[i];
 	}
-	for(int i = 0;i <= 12;i++){
-		cout << "The number that «C++ For Fools» sold in " << year[i]
+	for(int i = 0;i < 12;i++){
+		cout << "The number that C++ For Fools sold in " << year[i]
 		     << ": "<< num_sold[i] << endl;
+		total += num_sold[i];
 	}
+	cout << "The total: " << total;
 	
 	return 0;
 	
