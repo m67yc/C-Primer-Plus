16d8780b1661397ccec95e1e6f4b43cf32498a68
martinmcy <104663090+martinmcy@users.noreply.github.com>
2024-07-09 09:26:32 +0800

    Add files via upload

---
+++ CH5/ch5_ex1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int min,max,total;
+	
+	cout << "Enter two numbers that you want.\n";
+	cout << "the min:";
+	cin >> min;
+	cout << "the max:";
+	cin >> max;
+	for(int i = min;i <= 9;i++){
+		total += i;
+	}
+	cout << total;
+	
+	return 0;
+	
+}
