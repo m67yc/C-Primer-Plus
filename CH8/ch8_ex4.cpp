9fecaaafdbc86068ffec7997c731f84e6f9862c6
m67yc <martinmcymay@gmail.com>
2024-09-18 20:48:40 +0800

    Update ch8_ex4.cpp

--- CH8/ch8_ex4.cpp
+++ CH8/ch8_ex4.cpp
@@ -7,13 +7,13 @@ struct stringy {
   int ct;
 };
 
-void set(string , char*);
-void show(string , int n = 0);
-void show(char* , int n = 0);
+void set(stringy , char*);
+void show(const stringy , int n = 1);
+void show(char* , int n = 1);
 
 int main() {
 	
-	string beany;
+	stringy beany;
 	char testing[] = "Reality isn't what it used to be.";
 	
 	set(beany, testing);
@@ -28,20 +28,26 @@ int main() {
 	return 0;
 }
 
-void set(string str, char* ch){
+void set(stringy strt, char* ch){
 	
-	
+	strcpy(strt.str, ch);
+	for(int i = 0;ch[i] == '\0';i++)
+		strt.ct = i;
 	
 }
 
-void show(string str, int n){
+void show(const stringy strt, int n){
 	
-	
+	for(int i = 0;i < n;i++){
+		cout << strt.str << endl;
+	}
 	
 }
 
 void show(char* ch, int n){
 	
-	
+	for(int i = 0;i < n;i++){
+		cout << ch << endl;
+	}
 	
 }
